dd6a06f7150dbbd1d3845feaa332f9657ad08b81
esase <alexermashev@gmail.com>
2015-10-21 10:19:21 +0600

    Add csrf in forms

--- module/Slideshow/config/module.config.install.php
+++ module/Slideshow/config/module.config.install.php
@@ -22,7 +22,7 @@
  */
 return [
     'compatable' => '2.3.0',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'vendor' => 'eSASe',
     'vendor_email' => 'alexermashev@gmail.com',
     'description' => 'Module allows to publish photos slideshow on the site',
